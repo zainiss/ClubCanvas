f1bb8944c6dee55935fad61cb599ad228d1f9d01
zainiss <41850412+zainiss@users.noreply.github.com>
2025-11-06 10:23:36 -0500

    made clubs class

---
+++ ClubCanvas/ClubCanvas.mvc/Models/Club.cs
@@ -0,0 +1,12 @@
+namespace ClubCanvas.mvc.Models;
+
+public class Club
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Events { get; set; }
+    public List<string> Members { get; set; }
+    public string Owner { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}
